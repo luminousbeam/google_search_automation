a4a45448e306bb2ae99eb0d46c3950d7d0275f41
luminousbeam <jocelyn.jeriah@gmail.com>
2015-12-18 15:39:22 -0500

    updated and corrected to the modules I am going to be using, changed class parameter name to object, deleted the Selenium tests that were created, commented out the last line for now.

--- google_search.py
+++ google_search.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.support import expected_conditions as EC
+import logging
 
-class GoogleSearch(unittest.TestCase):
+class GoogleSearch(object):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -28,30 +26,4 @@ class GoogleSearch(unittest.TestCase):
         driver.find_element_by_link_text("Python - Overview").click()
         driver.find_element_by_link_text("Python - Dictionary").click()
 
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
+#if __name__ == "__main__":
